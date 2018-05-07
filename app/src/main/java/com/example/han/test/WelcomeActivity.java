13be04cd80e0301ee879e19c0b2a16ff84ba7ddc
han <moore840216@yahoo.com.tw>
2018-05-07 19:28:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/han/test/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.han.test;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        //隐藏掉整个ActionBar，包括下面的Tabs
+
+        getSupportActionBar().hide();
+
+        mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 2000); //2秒跳轉
+    }
+    private static final int GOTO_MAIN_ACTIVITY = 0;
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+
+            switch (msg.what) {
+                case GOTO_MAIN_ACTIVITY:
+                    Intent intent = new Intent();
+                    //將原本Activity的換成MainActivity
+                    intent.setClass(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    };
+}
